91258cb470231db6ea04ae1a140de1af76a8af8e
JoseDabas <josearieldabas01@gmail.com>
2024-04-04 23:41:38 -0500

    User

--- Profesores_WEB/js/user.js
+++ Profesores_WEB/js/user.js
@@ -1,1 +1,35 @@
-//Forgot your password
+//Boton submit del Log In
+document.getElementById("btnSubmit").addEventListener("click", () => {
+        // Obtener los valores de los campos del formulario
+        var username = document.getElementById("logname").value;
+        var email = document.getElementById("logemail").value;
+        var password = document.getElementById("logpass").value;
+        var confirmPassword = document.getElementById("logverifypass").value;
+        var career = document.getElementById("opcionesCareer").value;
+
+        // Crear un objeto con los datos del formulario
+        var formData = {
+            username: username,
+            email: email,
+            password: password,
+            confirmPassword: confirmPassword,
+            career: career
+        };
+
+        // Enviar los datos 
+        fetch("https://ejemplo.com/api/signup", {
+            method: "POST",
+            body: JSON.stringify(formData),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data); // Imprimir la respuesta del servidor en la consola
+            })
+            .catch(error => {
+                console.error("Error:", error);
+            });
+
+    });
--- server/routes/users.js
+++ server/routes/users.js
@@ -7,3 +7,8 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+//Boton de Submit en Sign Up
+
+
+
