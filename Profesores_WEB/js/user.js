d7216fe5bee878eea3286029f41fba2bdc730b5c
JoseDabas <josearieldabas01@gmail.com>
2024-04-05 02:37:34 -0500

    User

--- Profesores_WEB/js/user.js
+++ Profesores_WEB/js/user.js
@@ -1,36 +1,60 @@
-<<<<<<< HEAD
-//Boton submit del Log In
-document.getElementById("btnSubmit").addEventListener("click", () => {
-        // Obtener los valores de los campos del formulario
-        var username = document.getElementById("logname").value;
-        var email = document.getElementById("logemail").value;
-        var password = document.getElementById("logpass").value;
-        var confirmPassword = document.getElementById("logverifypass").value;
-        var career = document.getElementById("opcionesCareer").value;
+//Boton Submit para Sign UP
+const formCreateUser = document.getElementById('form-create-user');
+const signname = document.getElementById("signname").value
+const signemail = document.getElementById("signemail").value
+const signpass = document.getElementById("signpass").value
+const signverifypass = document.getElementById("signverifypass").value
+const opcionesCareer = document.getElementById("opcionesCareer").value
 
-        // Crear un objeto con los datos del formulario
-        var formData = {
-            username: username,
-            email: email,
-            password: password,
-            confirmPassword: confirmPassword,
-            career: career
-        };
 
-        // Enviar los datos 
-        fetch("https://ejemplo.com/api/signup", {
-            method: "POST",
-            body: JSON.stringify(formData),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data); // Imprimir la respuesta del servidor en la consola
-            })
-            .catch(error => {
-                console.error("Error:", error);
-            });
 
+formCreateUser.addEventListener('Submit', function(event){
+    event.preventDefault();
+
+  
+    const data = {
+        signname: signname,
+        signemail: signemail,
+        signpass: signpass,
+        signverifypass: signverifypass,
+        opcionesCareer: opcionesCareer,
+    }
+
+    if (opcionesCareer === '' && opcionesCareer === "Choose Your Career" ) {
+        alert('Por favor, selecciona una Carrera');
+    }
+    
+
+    if (signverifypass !== signpass)
+    {
+        alert("Las contraseñas no coinciden");
+    }
+
+    window.location.href = '';
+
+
+
+});
+
+
+//Manda el usuario al servidor
+function getUsers(){
+    axios({
+        method: 'get', 
+        url: 'http://localhost:3330/users/list',
+    }).then( response => {
+        console.log('response', response)
+    }).catch( error => {
+        console.log('error', error);
     });
+}
+
+getUsers();
+
+function redirectToPage() {
+    // Cambiar "pagina_destino.html" con la URL de la página a la que deseas redirigir
+    window.location.href = "index.html";
+}
+
+// Agregar un evento de clic al botón para llamar a la función de redirección
+document.getElementById("Submit").addEventListener("click", redirectToPage);
